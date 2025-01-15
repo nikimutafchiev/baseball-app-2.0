bc7c75997af949ae7a668b028906c9a48f947a81
Nikolay Mutafchiev <niki_mutafchiev@yahoo.com>
2025-01-15 21:58:11 +0200

    Secret flask key + jwt expiration

--- backend/app.py
+++ backend/app.py
@@ -5,12 +5,16 @@ from flask_jwt_extended import  JWTManager
 from models.models import db
 from routes.routes import route_bp
 from sqlalchemy import text
+from datetime import timedelta
+import uuid
+
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
-app.config['SECRET_KEY'] = 'mcskdcmk2mkd23kmdkkm33ksodcoexsd'
+app.config['SECRET_KEY'] = uuid.uuid4().hex
 app.config["JWT_SECRET_KEY"] = 'csimdcisdmc5d1eaaa59e6480eb1ff546458903fc90551fb4dbef1f0ef204f18aaf903a39e9'
 app.config['JWT_TOKEN_LOCATION'] = ['headers']
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(days=2)
 CORS(app)
 
 db.init_app(app)
